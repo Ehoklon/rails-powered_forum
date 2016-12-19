231324dc483ed0b5e20d8ef78b5bbe35e2211fc2
Egor Dubrovin <dubrovin.en@gmail.com>
2016-12-19 19:20:19 +0300

    Add post and comment models tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save empty' do
+    post = Post.new
+    assert !post.save
+  end
+
+  test 'should not save with unexist user' do
+    post = Post.new do |post|
+      post.content = 'Test Content'
+      post.user_id = 0
+    end
+    assert !post.save
+  end
+
+  test 'should save valid post' do
+    post = Post.new do |post|
+      post.content = 'Test Content'
+      post.user_id = users(:test_user).id
+    end
+    assert post.save
+  end
 end
