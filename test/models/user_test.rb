7dcf5a9f6d2db6928fc0625c031c251541fb2ee6
Egor Dubrovin <dubrovin.en@gmail.com>
2016-12-19 19:09:48 +0300

    Add user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save empty' do
+  user = User.new
+  assert !user.save
+end
+
+test 'should not save without email' do
+  user = User.new do |user|
+    user.email = ''
+    user.password = 'password'
+    user.password_confirmation = 'password'
+  end
+  assert !user.save
+end
+
+test 'should not save without password' do
+  user = User.new do |user|
+    user.email = 'example@test.com'
+    user.encrypted_password = ''
+  end
+  assert !user.save
+end
+
+test 'should not save already exist' do
+  test_user = users(:test_user)
+  user = User.new do |user|
+    user.email = test_user.email
+    user.password = 'password'
+    user.password_confirmation = 'password'
+  end
+  assert !user.save
+end
+
+test 'should save valid user' do
+  user = User.new do |user|
+    user.email = 'example@test.com'
+    user.password = 'password'
+    user.password_confirmation = 'password'
+  end
+  assert user.save
+end
 end
