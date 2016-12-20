e46d456bd3fe2918b432ddf54a125b5feacf1415
Egor Dubrovin <dubrovin.en@gmail.com>
2016-12-20 17:29:53 +0300

    Add integration tests

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -6,10 +6,12 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
   end
 
   test 'sign up' do
-    post user_registration_path, params:
-      { 'user[email]' => 'email@example.com',
-        'user[password]' => 'password',
-        'user[password_confirmation]' => 'password' }
+    post user_registration_path, params: {
+      user:
+      { email: 'email@example.com',
+        password: 'password',
+        password_confirmation: 'password' }
+    }
 
     assert_redirected_to root_path
 
@@ -17,16 +19,85 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
   end
 
   test 'sign in and write an post' do
-    post user_session_path, params:
-      { 'user[email]' => @test_user.email,
-        'user[password]' => 'password' }
+    post user_session_path, params: {
+      user:
+        { email: @test_user.email,
+        password: 'password' }
+    }
 
     assert_redirected_to root_path
 
-    put new_post_path, params: { 'post[content]' => 'Test Post' }
-    assert_redirected_to root_path
+    post posts_path, params: {
+      post:
+      { title: 'Post Title',
+        content: 'Post Content' }
+    }
 
-    assert Post.where(user_id: @test_user.id, content: 'Test Post')
+    assert post = Post.find_by(user_id: @test_user.id, content: 'Post Content')
+    assert_redirected_to '/posts/' + post.id.to_s
   end
 
+  test 'sign in and write a comment' do
+    post user_session_path, params: {
+      user:
+        { email: @test_user.email,
+        password: 'password' }
+    }
+
+    assert_redirected_to root_path
+
+    post post_comments_path(post_id: posts(:test_post).id), params: {
+      comment:
+      { comment: 'The Comment' }
+    }
+
+    assert Comment.find_by(user_id: @test_user.id, comment: 'The Comment', post_id: posts(:test_post).id)
+  end
+
+  test 'fail sign up' do
+    post user_registration_path, params: {
+      user:
+      { email: 'email@example.com',
+        password: 'password',
+        password_confirmation: 'password1' }
+    }
+
+    assert_response :success # Controller didn't redirect to root_path
+  end
+
+
+  test 'sign in and fail to write an post' do
+    post user_session_path, params: {
+      user:
+        { email: @test_user.email,
+        password: 'password' }
+    }
+
+    assert_redirected_to root_path
+
+    post posts_path, params: {
+      post:
+      { title: 'Post Title',
+        content: '' }
+    }
+
+    assert !Post.exists?(user_id: @test_user.id, content: 'Post Content')
+  end
+
+  test 'sign in and fail to write a comment' do
+    post user_session_path, params: {
+      user:
+        { email: @test_user.email,
+        password: 'password' }
+    }
+
+    assert_redirected_to root_path
+
+    post post_comments_path(post_id: posts(:test_post).id), params: {
+      comment:
+      { comment: '' }
+    }
+
+    assert !Comment.exists?(user_id: @test_user.id, comment: 'The Comment', post_id: posts(:test_post).id)
+  end
 end
